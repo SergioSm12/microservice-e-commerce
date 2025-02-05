7893042727a54926b72bddabca01fbfa28449453
Sergio Mesa <davidmesab7@gmail.com>
2025-02-04 20:49:49 -0500

    first commit and add msvc-catalog

---
+++ msvc-catalog/src/main/java/com/sergio/springcloud/msvc_catalog/MsvcCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.sergio.springcloud.msvc_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcCatalogApplication.class, args);
+	}
+
+}
---
+++ msvc-catalog/src/test/java/com/sergio/springcloud/msvc_catalog/MsvcCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergio.springcloud.msvc_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
